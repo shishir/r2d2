89e863b99e3c1c510c325770fd22050ce0fec95a
shishir <shishir.das@gmail.com>
2013-11-17 12:18:23 +0530

    Add #constantize method to aid in input parsing

---
+++ lib/r2d2/core_ext/extensions.rb
@@ -0,0 +1,27 @@
+String.class_eval do
+	def constantize
+	  names = self.split('::')
+	  names.shift if names.empty? || names.first.empty?
+
+	  names.inject(Object) do |constant, name|
+	    if constant == Object
+	      constant.const_get(name)
+	    else
+	      candidate = constant.const_get(name)
+	      next candidate if constant.const_defined?(name, false)
+	      next candidate unless Object.const_defined?(name)
+
+	      # Go down the ancestors to check it it's owned
+	      # directly before we reach Object or the end of ancestors.
+	      constant = constant.ancestors.inject do |const, ancestor|
+	        break const    if ancestor == Object
+	        break ancestor if ancestor.const_defined?(name, false)
+	        const
+	      end
+
+	      # owner is in Object, so raise
+	      constant.const_get(name, false)
+	    end
+	  end
+	end
+end
---
+++ spec/extensions_spec.rb
@@ -0,0 +1,16 @@
+require_relative('./spec_helper')
+
+describe "String extensions" do
+
+	class Dummy
+	end
+
+	it "should respond to #contantize" do
+		"String".should respond_to(:constantize)
+	end
+
+	it "should return the constant" do
+		"Dummy".constantize.should == Dummy
+	end
+
+end
